1d92a2b8b7bed091c31e6c2a19623ff129a63362
rgarcia <voodoorai2000@gmail.com>
2018-01-23 22:54:00 +0100

    Making rake db:dev_seed available in specs

--- spec/lib/tasks/dev_seed_spec.rb
+++ spec/lib/tasks/dev_seed_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 require 'rake'
 
 describe 'rake db:dev_seed' do
-  let :run_rake_task do
-    Rake.application.invoke_task('db:dev_seed')
-  end
+  before do
+     Rake.application.rake_require('tasks/db')
+     Rake::Task.define_task(:environment)
+   end
+
+   let :run_rake_task do
+      Rake::Task['db:dev_seed'].reenable
+      Rake.application.invoke_task('db:dev_seed')
+    end
 
   it 'seeds the database without errors' do
     expect { run_rake_task }.not_to raise_error
