3a388804c6e96ca042ebb500b0445fec4640f665
rgarcia <voodoorai2000@gmail.com>
2018-04-08 13:43:22 +0200

    Remove parameterise deprecation warning
    DEPRECATION WARNING: Passing the separator argument as a positional
    parameter is deprecated and will soon be removed. Use `separator: '_'`
    instead.

--- app/controllers/valuation/budget_investments_controller.rb
+++ app/controllers/valuation/budget_investments_controller.rb
@@ -63,7 +63,7 @@ class Valuation::BudgetInvestmentsController < Valuation::BaseController
     end
 
     def resource_name
-      resource_model.parameterize('_')
+      resource_model.parameterize(separator: '_')
     end
 
     def load_budget
