4eb3b6b085cf2b4c1947435ae4a948e6cecb87cd
Javi Martín <javim@elretirao.net>
2018-12-07 11:52:53 +0100

    Simplify code seeding settings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,115 +7,93 @@ if Administrator.count == 0 && !Rails.env.test?
   admin.create_administrator
 end
 
-# Names for the moderation console, as a hint for moderators
-# to know better how to assign users with official positions
-Setting["official_level_1_name"] = "Empleados públicos"
-Setting["official_level_2_name"] = "Organización Municipal"
-Setting["official_level_3_name"] = "Directores generales"
-Setting["official_level_4_name"] = "Concejales"
-Setting["official_level_5_name"] = "Alcaldesa"
+default_settings = {
+  # Names for the moderation console, as a hint for moderators
+  # to know better how to assign users with official positions
+  "official_level_1_name": "Empleados públicos",
+  "official_level_2_name": "Organización Municipal",
+  "official_level_3_name": "Directores generales",
+  "official_level_4_name": "Concejales",
+  "official_level_5_name": "Alcaldesa",
+  "max_ratio_anon_votes_on_debates": 50,
+  "max_votes_for_debate_edit": 1000,
+  "max_votes_for_proposal_edit": 1000,
+  "comments_body_max_length": 1000,
+  "proposal_code_prefix": "MAD",
+  "votes_for_proposal_success": 53726,
+  "months_to_archive_proposals": 12,
+  # Users with this email domain will automatically be marked as level 1 officials
+  # Emails under the domain's subdomains will also be included
+  "email_domain_for_officials": "",
+  # Code to be included at the top (inside <head>) of every page (useful for tracking)
+  "html.per_page_code_head": "",
+  "html.per_page_code_body": "", # Code to be included at the top (inside <body>) of every page
+  "twitter_handle": "abriendomadrid",
+  "twitter_hashtag": "#decidemadrid",
+  "facebook_handle": "Abriendo-Madrid-1475577616080350",
+  "youtube_handle": "channel/UCFmaChI9quIY7lwHplnacfg",
+  "telegram_handle": nil,
+  "instagram_handle": "decidemadrid",
+  "blog_url": "https://diario.madrid.es/decidemadrid/",
+  "transparency_url": "http://transparencia.madrid.es/",
+  "opendata_url": "http://datos.madrid.es/",
+  "url": "https://decide.madrid.es", # Public-facing URL of the app.
+  "org_name": "Decide Madrid", # Consul installation's organization name
+  "meta_title": nil,
+  "meta_description": nil,
+  "meta_keywords": nil,
+  "process.debates": true,
+  "process.proposals": true,
+  "process.polls": true,
+  "process.budgets": true,
+  "process.legislation": true,
+  "feature.featured_proposals": nil,
+  "feature.twitter_login": true,
+  "feature.facebook_login": true,
+  "feature.google_login": true,
+  "feature.public_stats": true,
+  "feature.signature_sheets": true,
+  "feature.user.recommendations": true,
+  "feature.user.recommendations_on_debates": true,
+  "feature.user.recommendations_on_proposals": true,
+  "feature.user.skip_verification": true,
+  "feature.community": true,
+  "feature.map": nil,
+  "feature.allow_images": true,
+  "feature.allow_attached_documents": true,
+  "feature.guides": true,
+  "feature.help_page": true,
+  "feature.captcha": nil,
+  "proposal_notification_minimum_interval_in_days": 3,
+  "direct_message_max_per_day": 3, # For proposal notifications
+  "mailer_from_name": "CONSUL",
+  "mailer_from_address": "noreply@consul.dev",
+  "verification_offices_url": "http://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Atencion-al-ciudadano/Oficinas-de-Atencion-al-Ciudadano?vgnextfmt=default&vgnextchannel=5b99cde2e09a4310VgnVCM1000000b205a0aRCRD",
+  "min_age_to_participate": 16,
+  "featured_proposals_number": 3,
+  "map.latitude": 40.4332002,
+  "map.longitude": -3.7009591,
+  "map.zoom": 10,
+  "related_content_score_threshold": -0.3,
+  "homepage.widgets.feeds.proposals": true,
+  "homepage.widgets.feeds.debates": true,
+  "homepage.widgets.feeds.processes": true,
+  "hot_score_period_in_days": 31,
+  "proposals.successful_proposal_id": nil,
+  "proposals.poll_short_title": nil,
+  "proposals.poll_description": nil,
+  "proposals.poll_link": nil,
+  "proposals.email_short_title": nil,
+  "proposals.email_description": nil,
+  "proposals.poster_short_title": nil,
+  "proposals.poster_description": nil,
+  "dashboard.emails": nil,
+  "captcha.max_failed_login_attempts": 5
+}
 
-Setting["max_ratio_anon_votes_on_debates"] = 50
-
-Setting["max_votes_for_debate_edit"] = 1000
-
-Setting["max_votes_for_proposal_edit"] = 1000
-
-Setting["comments_body_max_length"] = 1000
-
-Setting["proposal_code_prefix"] = "MAD"
-
-Setting["votes_for_proposal_success"] = 53726
-
-Setting["months_to_archive_proposals"] = 12
-
-# Users with this email domain will automatically be marked as level 1 officials
-# Emails under the domain's subdomains will also be included
-Setting["email_domain_for_officials"] = ""
-
-# Code to be included at the top (inside <head>) of every page (useful for tracking)
-Setting["html.per_page_code_head"] = ""
-
-# Code to be included at the top (inside <body>) of every page
-Setting["html.per_page_code_body"] = ""
-
-Setting["twitter_handle"] = "abriendomadrid"
-Setting["twitter_hashtag"] = "#decidemadrid"
-Setting["facebook_handle"] = "Abriendo-Madrid-1475577616080350"
-Setting["youtube_handle"] = "channel/UCFmaChI9quIY7lwHplnacfg"
-Setting["telegram_handle"] = nil
-Setting["instagram_handle"] = "decidemadrid"
-Setting["blog_url"] = "https://diario.madrid.es/decidemadrid/"
-Setting["transparency_url"] = "http://transparencia.madrid.es/"
-Setting["opendata_url"] = "http://datos.madrid.es/"
-
-Setting["url"] = "https://decide.madrid.es" # Public-facing URL of the app.
-
-# Consul installation's organization name
-Setting["org_name"] = "Decide Madrid"
-
-Setting["meta_title"] = nil
-Setting["meta_description"] = nil
-Setting["meta_keywords"] = nil
-
-Setting["process.debates"] = true
-Setting["process.proposals"] = true
-Setting["process.polls"] = true
-Setting["process.budgets"] = true
-Setting["process.legislation"] = true
-
-Setting["feature.featured_proposals"] = nil
-Setting["feature.twitter_login"] = true
-Setting["feature.facebook_login"] = true
-Setting["feature.google_login"] = true
-Setting["feature.public_stats"] = true
-Setting["feature.signature_sheets"] = true
-Setting["feature.user.recommendations"] = true
-Setting["feature.user.recommendations_on_debates"] = true
-Setting["feature.user.recommendations_on_proposals"] = true
-Setting["feature.user.skip_verification"] = true
-Setting["feature.community"] = true
-Setting["feature.map"] = nil
-Setting["feature.allow_images"] = true
-Setting["feature.allow_attached_documents"] = true
-Setting["feature.guides"] = true
-Setting["feature.help_page"] = true
-Setting["feature.captcha"] = nil
-
-Setting["proposal_notification_minimum_interval_in_days"] = 3
-Setting["direct_message_max_per_day"] = 3 # For proposal notifications
-
-Setting["mailer_from_name"] = "CONSUL"
-Setting["mailer_from_address"] = "noreply@consul.dev"
-
-Setting["verification_offices_url"] = "http://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Atencion-al-ciudadano/Oficinas-de-Atencion-al-Ciudadano?vgnextfmt=default&vgnextchannel=5b99cde2e09a4310VgnVCM1000000b205a0aRCRD"
-Setting["min_age_to_participate"] = 16
-
-Setting["featured_proposals_number"] = 3
-
-Setting["map.latitude"] = 40.4332002
-Setting["map.longitude"] = -3.7009591
-Setting["map.zoom"] = 10
-
-Setting["related_content_score_threshold"] = -0.3
-Setting["homepage.widgets.feeds.proposals"] = true
-Setting["homepage.widgets.feeds.debates"] = true
-Setting["homepage.widgets.feeds.processes"] = true
-
-Setting["hot_score_period_in_days"] = 31
-
-Setting["proposals.successful_proposal_id"] = nil
-Setting["proposals.poll_short_title"] = nil
-Setting["proposals.poll_description"] = nil
-Setting["proposals.poll_link"] = nil
-Setting["proposals.email_short_title"] = nil
-Setting["proposals.email_description"] = nil
-Setting["proposals.poster_short_title"] = nil
-Setting["proposals.poster_description"] = nil
-
-Setting["dashboard.emails"] = nil
-
-Setting["captcha.max_failed_login_attempts"] = 5
+default_settings.each do |name, value|
+  Setting[name] = value
+end
 
 # Default custom pages
 load Rails.root.join("db", "pages.rb")
