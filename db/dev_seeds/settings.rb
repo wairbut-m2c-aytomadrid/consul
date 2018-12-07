adf73eff0bce71d7a7859d6ca77c3294c0c37bb5
Javi Martín <javim@elretirao.net>
2018-12-07 12:18:04 +0100

    Simplify dev seed settings code

--- db/dev_seeds/settings.rb
+++ db/dev_seeds/settings.rb
@@ -1,27 +1,28 @@
 section "Creating Settings" do
   Setting.reset_defaults
 
-  Setting["email_domain_for_officials"] = "madrid.es"
-  Setting.create(key: "votes_for_proposal_success", value: "100")
-
-  Setting.create(key: "twitter_handle", value: "@decidemadrid")
-  Setting.create(key: "facebook_handle", value: "decidemadrid")
-  Setting.create(key: "youtube_handle", value: "decidemadrid")
-  Setting.create(key: "telegram_handle", value: "decidemadrid")
-  Setting.create(key: "url", value: "http://localhost:3000")
-
-  Setting.create(key: "feature.probe.plaza", value: "true")
-  Setting.create(key: "feature.human_rights.accepting", value: "true")
-  Setting.create(key: "feature.human_rights.voting", value: "true")
-  Setting.create(key: "feature.human_rights.closed", value: "true")
-  Setting.create(key: "feature.user.skip_verification", value: "true")
-  Setting.create(key: "feature.map", value: "true")
-
-  Setting.create(key: "mailer_from_name", value: "Decide Madrid")
-  Setting.create(key: "mailer_from_address", value: "noreply@madrid.es")
-  Setting.create(key: "meta_title", value: "Decide Madrid")
-  Setting.create(key: "meta_description", value: "Citizen Participation & Open Gov Application")
-  Setting.create(key: "meta_keywords", value: "citizen participation, open government")
-  Setting.create(key: "verification_offices_url", value: "http://oficinas-atencion-ciudadano.url/")
-  Setting.create(key: "proposal_notification_minimum_interval_in_days", value: 0)
+  {
+    "email_domain_for_officials": "madrid.es",
+    "facebook_handle": "decidemadrid",
+    "feature.human_rights.accepting": "true",
+    "feature.human_rights.closed": "true",
+    "feature.human_rights.voting": "true",
+    "feature.map": "true",
+    "feature.probe.plaza": "true",
+    "feature.user.skip_verification": "true",
+    "mailer_from_address": "noreply@madrid.es",
+    "mailer_from_name": "Decide Madrid",
+    "meta_description": "Citizen Participation & Open Gov Application",
+    "meta_keywords": "citizen participation, open government",
+    "meta_title": "Decide Madrid",
+    "proposal_notification_minimum_interval_in_days": 0,
+    "telegram_handle": "decidemadrid",
+    "twitter_handle": "@decidemadrid",
+    "url": "http://localhost:3000",
+    "verification_offices_url": "http://oficinas-atencion-ciudadano.url/",
+    "votes_for_proposal_success": "100",
+    "youtube_handle": "decidemadrid"
+  }.each do |name, value|
+    Setting[name] = value
+  end
 end
