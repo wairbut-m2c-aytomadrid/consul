2b2b951074af214272df9ca68f17519e36b1b6b9
voodoorai2000 <voodoorai2000@gmail.com>
2019-02-25 21:07:30 +0100

    Migrate votes for hidden users

--- lib/migrations/spending_proposal/vote.rb
+++ lib/migrations/spending_proposal/vote.rb
@@ -21,11 +21,25 @@ class Migrations::SpendingProposal::Vote
       budget_investment = find_budget_investment(vote.votable)
       return false unless budget_investment
 
-      if budget_investment.vote_by(voter: vote.voter, vote: "yes")
+      if vote.voter
+        budget_investment.vote_by(voter: vote.voter, vote: "yes")
+        log(".")
+      elsif vote_with_hidden_user(vote.voter_id, budget_investment)
         log(".")
       else
         log("\nError creating budget investment vote from spending proposal vote: #{vote.id}\n")
       end
+    end
+
+    def vote_with_hidden_user(voter_id, budget_investment)
+      vote_attributes = {
+        voter_id: voter_id,
+        votable: budget_investment,
+        vote_flag: true
+      }
+
+      vote = Vote.where(vote_attributes).first_or_create
+      vote.save(validate: false)
     end
 
 end
