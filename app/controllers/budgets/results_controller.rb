5bfb69f33a827a3e1c06c2f31e09def80ed0d4e1
voodoorai2000 <voodoorai2000@gmail.com>
2019-03-07 22:11:38 +0100

    Use city heading as default for results
    The 2016 budget's first heading created was for the district "Villa de Vallecas".
    Therefore that was the heading that was displayed when going to the results page,
    without a `heading_id` parameter.
    
    With this commit we are making the city heading[1] the default heading displayed.
    
    [1] https://github.com/AyuntamientoMadrid/consul/blob/master/app/models/budget.rb#L184

--- app/controllers/budgets/results_controller.rb
+++ app/controllers/budgets/results_controller.rb
@@ -18,9 +18,12 @@ module Budgets
 
       def load_heading
         if @budget.present?
-          headings = @budget.headings
-          @heading = headings.find_by_slug_or_id(params[:heading_id]) || headings.first
+          @heading = @budget.headings.find_by_slug_or_id(params[:heading_id]) || default_heading
         end
+      end
+
+      def default_heading
+        @budget.city_heading || @budget.headings.first
       end
 
   end
