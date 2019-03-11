d8ecd8ce41ca586da467be3dcc11625601172d41
decabeza <alberto@decabeza.es>
2019-03-11 14:06:19 +0100

    Remove analytics url setting

--- db/dev_seeds/settings.rb
+++ db/dev_seeds/settings.rb
@@ -80,7 +80,6 @@ section "Creating Settings" do
   Setting.create(key: "direct_message_max_per_day", value: 3)
   Setting.create(key: "related_content_score_threshold", value: -0.3)
   Setting.create(key: "hot_score_period_in_days", value: 31)
-  Setting.create(key: "analytics_url", value: "")
 
   # piwik_tracking_code_head = "<!-- Piwik -->
   # <script type='text/javascript'>
