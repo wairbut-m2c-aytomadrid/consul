086aa5a67062996236fb29146aecf07b86360cce
decabeza <alberto@decabeza.es>
2019-03-11 23:22:57 +0100

    Remove duplicated min age to verify
    There is a setting min_age_to_participate with the same behaviour.

--- app/models/user.rb
+++ app/models/user.rb
@@ -285,11 +285,7 @@ class User < ActiveRecord::Base
   end
 
   def self.minimum_required_age
-    (Setting['min_age_to_participate'] || 16).to_i
-  end
-
-  def self.minimum_required_age_for_verification
-    (Setting['min_age_to_verify'] || 16).to_i
+    (Setting["min_age_to_participate"] || 16).to_i
   end
 
   def show_welcome_screen?
