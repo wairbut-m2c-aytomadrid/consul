consuldemocracy/consuldemocracy

Fix hound warnigns on seeds

db/seeds.rb
@@ -1,7 +1,9 @@
 # coding: utf-8
 # Default admin user (change password after first deploy to a server!)
 if Administrator.count == 0 && !Rails.env.test?
-  admin = User.create!(username: 'admin', email: 'admin@consul.dev', password: '12345678', password_confirmation: '12345678', confirmed_at: Time.current, terms_of_service: "1")
+  admin = User.create!(username: "admin", email: "admin@consul.dev", password: "12345678",
+                       password_confirmation: "12345678", confirmed_at: Time.current,
+                       terms_of_service: "1")
   admin.create_administrator
 end
 
@@ -23,10 +25,10 @@ Setting["max_votes_for_debate_edit"] = 1000
 Setting["max_votes_for_proposal_edit"] = 1000
 
 # Max length for comments
-Setting['comments_body_max_length'] = 1000
+Setting["comments_body_max_length"] = 1000
 
 # Prefix for the Proposal codes
-Setting["proposal_code_prefix"] = 'CONSUL'
+Setting["proposal_code_prefix"] = "CONSUL"
 
 # Number of votes needed for proposal success
 Setting["votes_for_proposal_success"] = 53726
@@ -36,13 +38,13 @@ Setting["months_to_archive_proposals"] = 12
 
 # Users with this email domain will automatically be marked as level 1 officials
 # Emails under the domain's subdomains will also be included
-Setting["email_domain_for_officials"] = ''
+Setting["email_domain_for_officials"] = ""
 
 # Code to be included at the top (inside <head>) of every page (useful for tracking)
-Setting['per_page_code_head'] = ''
+Setting["per_page_code_head"] = ""
 
 # Code to be included at the top (inside <body>) of every page
-Setting['per_page_code_body'] = ''
+Setting["per_page_code_body"] = ""
 
 # Social settings
 Setting["twitter_handle"] = nil
@@ -64,63 +66,63 @@ Setting["meta_description"] = nil
 Setting["meta_keywords"] = nil
 
 # Feature flags
-Setting['feature.debates'] = true
-Setting['feature.proposals'] = true
-Setting['feature.spending_proposals'] = nil
-Setting['feature.polls'] = true
-Setting['feature.twitter_login'] = true
-Setting['feature.facebook_login'] = true
-Setting['feature.google_login'] = true
-Setting['feature.public_stats'] = true
-Setting['feature.budgets'] = true
-Setting['feature.signature_sheets'] = true
-Setting['feature.legislation'] = true
-Setting['feature.user.recommendations'] = true
-Setting['feature.user.recommendations_on_debates'] = true
-Setting['feature.user.recommendations_on_proposals'] = true
-Setting['feature.community'] = true
-Setting['feature.map'] = nil
-Setting['feature.allow_images'] = true
-Setting['feature.allow_attached_documents'] = true
-Setting['feature.help_page'] = true
+Setting["feature.debates"] = true
+Setting["feature.proposals"] = true
 Setting["feature.featured_proposals"] = nil
+Setting["feature.spending_proposals"] = nil
+Setting["feature.polls"] = true
+Setting["feature.twitter_login"] = true
+Setting["feature.facebook_login"] = true
+Setting["feature.google_login"] = true
+Setting["feature.public_stats"] = true
+Setting["feature.budgets"] = true
+Setting["feature.signature_sheets"] = true
+Setting["feature.legislation"] = true
+Setting["feature.user.recommendations"] = true
+Setting["feature.user.recommendations_on_debates"] = true
+Setting["feature.user.recommendations_on_proposals"] = true
+Setting["feature.community"] = true
+Setting["feature.map"] = nil
+Setting["feature.allow_images"] = true
+Setting["feature.allow_attached_documents"] = true
+Setting["feature.help_page"] = true
 
 # Spending proposals feature flags
-Setting['feature.spending_proposal_features.voting_allowed'] = nil
+Setting["feature.spending_proposal_features.voting_allowed"] = nil
 
 # Proposal notifications
-Setting['proposal_notification_minimum_interval_in_days'] = 3
-Setting['direct_message_max_per_day'] = 3
+Setting["proposal_notification_minimum_interval_in_days"] = 3
+Setting["direct_message_max_per_day"] = 3
 
 # Email settings
-Setting['mailer_from_name'] = 'CONSUL'
-Setting['mailer_from_address'] = 'noreply@consul.dev'
+Setting["mailer_from_name"] = "CONSUL"
+Setting["mailer_from_address"] = "noreply@consul.dev"
 
 # Verification settings
-Setting['min_age_to_participate'] = 16
+Setting["min_age_to_participate"] = 16
 
 # Featured proposals
-Setting['featured_proposals_number'] = 3
+Setting["featured_proposals_number"] = 3
 
 # City map feature default configuration (Greenwich)
-Setting['map_latitude'] = 51.48
-Setting['map_longitude'] = 0.0
-Setting['map_zoom'] = 10
+Setting["map_latitude"] = 51.48
+Setting["map_longitude"] = 0.0
+Setting["map_zoom"] = 10
 
 # Related content
-Setting['related_content_score_threshold'] = -0.3
+Setting["related_content_score_threshold"] = -0.3
 
-Setting["feature.user.skip_verification"] = 'true'
+Setting["feature.user.skip_verification"] = "true"
 
-Setting['feature.homepage.widgets.feeds.proposals'] = true
-Setting['feature.homepage.widgets.feeds.debates'] = true
-Setting['feature.homepage.widgets.feeds.processes'] = true
+Setting["feature.homepage.widgets.feeds.proposals"] = true
+Setting["feature.homepage.widgets.feeds.debates"] = true
+Setting["feature.homepage.widgets.feeds.processes"] = true
 
 # Votes hot_score configuration
-Setting['hot_score_period_in_days'] = 31
+Setting["hot_score_period_in_days"] = 31
 
-WebSection.create(name: 'homepage')
-WebSection.create(name: 'debates')
-WebSection.create(name: 'proposals')
-WebSection.create(name: 'budgets')
-WebSection.create(name: 'help_page')
+WebSection.create(name: "homepage")
+WebSection.create(name: "debates")
+WebSection.create(name: "proposals")
+WebSection.create(name: "budgets")
+WebSection.create(name: "help_page")

wairbut-m2c-aytomadrid/consul

db/seeds.rb
@@ -1,7 +1,9 @@
 # coding: utf-8
 # Default admin user (change password after first deploy to a server!)
 if Administrator.count == 0 && !Rails.env.test?
-  admin = User.create!(username: 'admin', email: 'admin@consul.dev', password: '12345678', password_confirmation: '12345678', confirmed_at: Time.current, terms_of_service: "1")
+  admin = User.create!(username: "admin", email: "admin@consul.dev", password: "12345678",
+                       password_confirmation: "12345678", confirmed_at: Time.current,
+                       terms_of_service: "1")
   admin.create_administrator
 end
 
@@ -23,10 +25,10 @@ Setting["max_votes_for_debate_edit"] = 1000
 Setting["max_votes_for_proposal_edit"] = 1000
 
 # Max length for comments
-Setting['comments_body_max_length'] = 1000
+Setting["comments_body_max_length"] = 1000
 
 # Prefix for the Proposal codes
-Setting["proposal_code_prefix"] = 'MAD'
+Setting["proposal_code_prefix"] = "MAD"
 
 # Number of votes needed for proposal success
 Setting["votes_for_proposal_success"] = 53726
@@ -36,7 +38,7 @@ Setting["months_to_archive_proposals"] = 12
 
 # Users with this email domain will automatically be marked as level 1 officials
 # Emails under the domain's subdomains will also be included
-Setting["email_domain_for_officials"] = ''
+Setting["email_domain_for_officials"] = ""
 
 # Code to be included at the top (inside <head>) of every page (useful for tracking)
 Setting["html.per_page_code_head"] = ""
@@ -75,50 +77,50 @@ Setting["process.legislation"] = true
 
 # Feature flags
 Setting["feature.featured_proposals"] = nil
-Setting['feature.spending_proposals'] = nil
-Setting['feature.twitter_login'] = true
-Setting['feature.facebook_login'] = true
-Setting['feature.google_login'] = true
-Setting['feature.public_stats'] = true
-Setting['feature.signature_sheets'] = true
-Setting['feature.user.recommendations'] = true
-Setting['feature.user.recommendations_on_debates'] = true
-Setting['feature.user.recommendations_on_proposals'] = true
+Setting["feature.spending_proposals"] = nil
+Setting["feature.twitter_login"] = true
+Setting["feature.facebook_login"] = true
+Setting["feature.google_login"] = true
+Setting["feature.public_stats"] = true
+Setting["feature.signature_sheets"] = true
+Setting["feature.user.recommendations"] = true
+Setting["feature.user.recommendations_on_debates"] = true
+Setting["feature.user.recommendations_on_proposals"] = true
 Setting["feature.user.skip_verification"] = true
-Setting['feature.community'] = true
-Setting['feature.map'] = nil
-Setting['feature.allow_images'] = true
-Setting['feature.allow_attached_documents'] = true
-Setting['feature.guides'] = nil
-Setting['feature.help_page'] = true
+Setting["feature.community"] = true
+Setting["feature.map"] = nil
+Setting["feature.allow_images"] = true
+Setting["feature.allow_attached_documents"] = true
+Setting["feature.guides"] = true
+Setting["feature.help_page"] = true
 
 # Spending proposals feature flags
-Setting['feature.spending_proposal_features.phase1'] = true
-Setting['feature.spending_proposal_features.phase2'] = nil
-Setting['feature.spending_proposal_features.phase3'] = nil
-Setting['feature.spending_proposal_features.voting_allowed'] = nil
-Setting['feature.spending_proposal_features.final_voting_allowed'] = true
-Setting['feature.spending_proposal_features.open_results_page'] = nil
-Setting['feature.spending_proposal_features.valuation_allowed'] = nil
+Setting["feature.spending_proposal_features.phase1"] = true
+Setting["feature.spending_proposal_features.phase2"] = nil
+Setting["feature.spending_proposal_features.phase3"] = nil
+Setting["feature.spending_proposal_features.voting_allowed"] = nil
+Setting["feature.spending_proposal_features.final_voting_allowed"] = true
+Setting["feature.spending_proposal_features.open_results_page"] = nil
+Setting["feature.spending_proposal_features.valuation_allowed"] = nil
 
 # Proposal notifications
-Setting['proposal_notification_minimum_interval_in_days'] = 3
-Setting['direct_message_max_per_day'] = 3
+Setting["proposal_notification_minimum_interval_in_days"] = 3
+Setting["direct_message_max_per_day"] = 3
 
 # Email settings
-Setting['mailer_from_name'] = 'CONSUL'
-Setting['mailer_from_address'] = 'noreply@consul.dev'
+Setting["mailer_from_name"] = "CONSUL"
+Setting["mailer_from_address"] = "noreply@consul.dev"
 
 # Verification settings
-Setting['verification_offices_url'] = 'http://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Atencion-al-ciudadano/Oficinas-de-Atencion-al-Ciudadano?vgnextfmt=default&vgnextchannel=5b99cde2e09a4310VgnVCM1000000b205a0aRCRD'
-Setting['min_age_to_participate'] = 16
-Setting['min_age_to_verify'] = 16
+Setting["verification_offices_url"] = "http://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Atencion-al-ciudadano/Oficinas-de-Atencion-al-Ciudadano?vgnextfmt=default&vgnextchannel=5b99cde2e09a4310VgnVCM1000000b205a0aRCRD"
+Setting["min_age_to_participate"] = 16
+Setting["min_age_to_verify"] = 16
 
 # Featured proposals
-Setting['featured_proposals_number'] = 3
+Setting["featured_proposals_number"] = 3
 
 # Proposal improvement url path ('/help/proposal-improvement')
-Setting['proposal_improvement_path'] = nil
+Setting["proposal_improvement_path"] = nil
 
 # City map feature default configuration (Greenwich)
 Setting["map.latitude"] = 40.4332002
@@ -126,7 +128,7 @@ Setting["map.longitude"] = -3.7009591
 Setting["map.zoom"] = 10
 
 # Related content
-Setting['related_content_score_threshold'] = -0.3
+Setting["related_content_score_threshold"] = -0.3
 
 # Homepage
 Setting["homepage.widgets.feeds.proposals"] = true