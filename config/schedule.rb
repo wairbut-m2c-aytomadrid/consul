e282a1c465fc84a9dee8c2ea8080245850c97080
decabeza <alberto@decabeza.es>
2019-04-15 14:30:10 +0200

    Temporally not send dashboard's notifications

--- config/schedule.rb
+++ config/schedule.rb
@@ -39,9 +39,10 @@ every 1.day, at: '4:00 am', roles: [:cron] do
   rake "csv:export"
 end
 
-every 1.day, at: "7:00 am" do
-  rake "dashboards:send_notifications"
-end
+# Temporally not send dashboard's notifications
+# every 1.day, at: "7:00 am" do
+#   rake "dashboards:send_notifications"
+# end
 
 every 1.day, at: "3:00 am", roles: [:cron] do
   rake "votes:reset_hot_score"
