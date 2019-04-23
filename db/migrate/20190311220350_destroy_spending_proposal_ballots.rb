a9b496acf65e0673424ab24a805b96b6c30ed4ad
Javi Martín <javim@elretirao.net>
2019-04-23 21:33:06 +0200

    Add Rails version to spending proposals migrations
    These migrations were created shortly before updating to Rails 5, and so
    they didn't have the version number.

--- db/migrate/20190311220350_destroy_spending_proposal_ballots.rb
+++ db/migrate/20190311220350_destroy_spending_proposal_ballots.rb
@@ -1,4 +1,4 @@
-class DestroySpendingProposalBallots < ActiveRecord::Migration
+class DestroySpendingProposalBallots < ActiveRecord::Migration[4.2]
   def change
     drop_table :ballot_lines
     drop_table :ballots
--- db/migrate/20190326211832_destroy_spending_proposal_associations.rb
+++ db/migrate/20190326211832_destroy_spending_proposal_associations.rb
@@ -1,4 +1,4 @@
-class DestroySpendingProposalAssociations < ActiveRecord::Migration
+class DestroySpendingProposalAssociations < ActiveRecord::Migration[4.2]
   def change
     remove_column :budget_investments, :original_spending_proposal_id
     remove_column :tags, :spending_proposals_count
