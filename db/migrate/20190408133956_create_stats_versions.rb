01d555964faa058aba01bd1495ec4fcb2539b12c
Javi Martín <javim@elretirao.net>
2019-04-24 11:32:31 +0200

    Use Rails 5 conventions in stats versions
    This model was added after the Rails 5 branch was created but before it
    was merged.

--- db/migrate/20190408133956_create_stats_versions.rb
+++ db/migrate/20190408133956_create_stats_versions.rb
@@ -1,4 +1,4 @@
-class CreateStatsVersions < ActiveRecord::Migration
+class CreateStatsVersions < ActiveRecord::Migration[4.2]
   def change
     create_table :stats_versions do |t|
       t.references :process, polymorphic: true, index: true
