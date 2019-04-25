69519c90218c922089541d6ee96f0700fcf3b384
voodoorai2000 <voodoorai2000@gmail.com>
2019-04-25 21:16:06 +0200

    Bring back Madrid production deploy configuration
    This file had been changed in the recently deployed macro PR for the dashboard[1]
    
    [1] https://github.com/AyuntamientoMadrid/consul/pull/1945/files#diff-3497bb8a52e6d846e0e545e8e1285ca8L1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
 set :deploy_to, deploysecret(:deploy_to)
 set :server_name, deploysecret(:server_name)
 set :db_server, deploysecret(:db_server)
-set :branch, :master
+set :branch, :stable
 set :ssh_options, port: deploysecret(:ssh_port)
 set :stage, :production
 set :rails_env, :production
 set :keep_releases, 5
 
-server deploysecret(:server1), user: deploysecret(:user), roles: %w(web app db importer cron background)
-#server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
-#server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
-#server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
+#server deploysecret(:server1), user: deploysecret(:user), roles: %w(web app db importer cron background)
+server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
+server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
+server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
