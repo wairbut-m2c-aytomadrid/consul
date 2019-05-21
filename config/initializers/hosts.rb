f858b161253fd8417c648cd00a9538b647510665
Julian Herrero <microweb10@gmail.com>
2019-05-21 15:28:10 +0200

    Use double quotes in config/initializers

--- config/initializers/hosts.rb
+++ config/initializers/hosts.rb
@@ -1,7 +1,7 @@
 if Rails.env.test?
-  Rails.application.routes.default_url_options = { host: 'test' }
+  Rails.application.routes.default_url_options = { host: "test" }
 elsif Rails.env.development?
-  Rails.application.routes.default_url_options = { host: 'localhost', port: 3000 }
+  Rails.application.routes.default_url_options = { host: "localhost", port: 3000 }
 else
   Rails.application.routes.default_url_options = { host: Rails.application.secrets.server_name }
 end
